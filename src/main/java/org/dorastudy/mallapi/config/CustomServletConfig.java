e05c67a3b01f130b6739416f2ff8d64a1e31f37a
dora <apricity36@gmail.com>
2024-07-23 10:09:52 +0900

    REST 컨트롤러 생성, RestControllerAdvice를 사용한 예외 처리, CORS 설정

---
+++ src/main/java/org/dorastudy/mallapi/config/CustomServletConfig.java
@@ -0,0 +1,25 @@
+package org.dorastudy.mallapi.config;
+
+import lombok.extern.log4j.Log4j2;
+import org.dorastudy.mallapi.controller.formatter.LocalDateFormatter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@Log4j2
+public class CustomServletConfig implements WebMvcConfigurer {
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+        registry.addFormatter(new LocalDateFormatter());
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .maxAge(500)
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS")
+                .allowedOrigins("*");
+    }
+}
