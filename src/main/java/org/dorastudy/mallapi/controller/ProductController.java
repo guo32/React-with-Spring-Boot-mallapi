34fe8a6aca9b77f14c07a120362ae6ae2074d34f
dora <apricity36@gmail.com>
2024-07-29 14:15:35 +0900

    파일 업로드 Thumbnail 처리

---
+++ src/main/java/org/dorastudy/mallapi/controller/ProductController.java
@@ -0,0 +1,32 @@
+package org.dorastudy.mallapi.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.dorastudy.mallapi.dto.ProductDTO;
+import org.dorastudy.mallapi.util.CustomFileUtil;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@Log4j2
+@RequiredArgsConstructor
+@RequestMapping("/api/products")
+public class ProductController {
+    private final CustomFileUtil fileUtil;
+
+    @PostMapping("/")
+    public Map<String, String> register(ProductDTO productDTO) {
+        log.info("register: {}", productDTO);
+        List<MultipartFile> files = productDTO.getFiles();
+        List<String> uploadedFileNames = fileUtil.saveFiles(files);
+        productDTO.setUploadedFileNames(uploadedFileNames);
+        log.info(uploadedFileNames);
+
+        return Map.of("RESULT", "SUCCESS");
+    }
+}
