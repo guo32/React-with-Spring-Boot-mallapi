b7f714a36da05379ee755717898ccba61404a06c
dora <apricity36@gmail.com>
2024-08-26 13:03:51 +0900

    로그인 성공 및 실패 처리

---
+++ src/main/java/org/dorastudy/mallapi/handler/APILoginFailHandler.java
@@ -0,0 +1,31 @@
+package org.dorastudy.mallapi.handler;
+
+import com.google.gson.Gson;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Map;
+
+@Log4j2
+public class APILoginFailHandler implements AuthenticationFailureHandler {
+
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+        log.info("Login failed: " + exception);
+
+        Gson gson = new Gson();
+        String jsonStr = gson.toJson(Map.of("error", "ERROR_LOGIN"));
+
+        // === 상태 코드를 지정하지 않으면 200
+        response.setContentType("application/json");
+        PrintWriter printWriter = response.getWriter();
+        printWriter.println(jsonStr);
+        printWriter.close();
+    }
+}
---
+++ src/main/java/org/dorastudy/mallapi/handler/APILoginSuccessHandler.java
@@ -0,0 +1,43 @@
+package org.dorastudy.mallapi.handler;
+
+import com.google.gson.Gson;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.log4j.Log4j2;
+import org.dorastudy.mallapi.dto.MemberDTO;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Map;
+
+@Log4j2
+public class APILoginSuccessHandler implements AuthenticationSuccessHandler {
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+        log.info("---------- onAuthenticationSuccess start ----------");
+        log.info(authentication);
+
+        // === 인증 데이터로부터 memberDTO 값 추출
+        MemberDTO memberDTO = (MemberDTO) authentication.getPrincipal();
+
+        // === Token 추가 (추가 정보)
+        Map<String, Object> claims = memberDTO.getClaims();
+        claims.put("accessToken", "");
+        claims.put("refreshToken", "");
+
+        // === json 생성
+        Gson gson = new Gson();
+        String jsonStr = gson.toJson(claims);
+
+        response.setContentType("application/json; charset=UTF-8");
+        PrintWriter printWriter = response.getWriter();
+        printWriter.println(jsonStr);
+        printWriter.close();
+
+        log.info("---------- onAuthenticationSuccess end ----------");
+    }
+}
